0c7f899042d4deed6e107dfc666d7be46abbec62
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-05 22:34:51 -0300

    Creating and configuring the app

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import "./global.css";
+
+import { Login } from "@/screens/Login";
+
+export default function App() {
+  return <Login />;
+}
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Login() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold">Login Screen</Text>
+    </View>
+  );
+}
