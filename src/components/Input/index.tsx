e36b0d06296e185fbdd1ace0ecce3fbb9d8da2b9
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-08 12:41:29 -0300

    Detecting input focus

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import {
   Text,
   TextInput,
@@ -5,6 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { useRef, useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
 
@@ -24,18 +26,48 @@ export function Input<T extends FieldValues>({
   leftIconName,
   ...rest
 }: InputProps<T>) {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef<TextInput>(null);
+
+  function onFocus() {
+    if (inputRef.current) {
+      setIsFocused(inputRef.current.isFocused);
+    }
+  }
+
   return (
     <Controller
       control={control}
       name={name}
       render={({ field: { onChange, value } }) => (
         <View className="w-full">
-          {label && <Text className="text-white">{label}</Text>}
+          {label && (
+            <Text
+              className={clsx(
+                "mt-3 mb-2 text-base uppercase",
+                isFocused ? "text-accent-brand" : "text-gray-600"
+              )}
+            >
+              {label}
+            </Text>
+          )}
 
-          <TouchableOpacity className="h-16 px-3 py-2 flex-row items-center justify-between border-b-[1px] border-gray-600">
+          <TouchableOpacity className="h-16 px-3 py-2 flex-row items-center justify-between gap-2 border-b-[1px] border-gray-600">
+            {leftIconName && (
+              <MaterialIcons
+                name={leftIconName}
+                size={24}
+                color={isFocused ? colors["accent-brand"] : colors.gray[600]}
+              />
+            )}
+
             <TextInput
+              ref={inputRef}
+              className="flex-1 text-base text-gray-500"
               value={value}
               onChangeText={onChange}
+              onFocus={onFocus}
+              onEndEditing={onFocus}
               placeholderTextColor={colors.gray[700]}
               {...rest}
             />
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 
 import { Input } from "../Input";
+import { View } from "react-native";
 
 export type FormLoginParams = {
   email: string;
@@ -14,11 +15,23 @@ export function LoginForm() {
     formState: { isSubmitting },
   } = useForm<FormLoginParams>();
   return (
-    <Input
-      label="Email"
-      control={control}
-      name="email"
-      placeholder="example@mail.com"
-    />
+    <View className="gap-4">
+      <Input
+        label="Email"
+        control={control}
+        name="email"
+        placeholder="Digite seu email"
+        leftIconName="mail-outline"
+      />
+
+      <Input
+        label="Senha"
+        control={control}
+        name="password"
+        placeholder="Sua senha"
+        leftIconName="mail-lock"
+        secureTextEntry
+      />
+    </View>
   );
 }
