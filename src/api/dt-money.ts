c746e339642abac3bc88aa454a1b21b04f812938
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-09 16:04:52 -0300

    Creating AppError

--- src/api/dt-money.ts
+++ src/api/dt-money.ts
@@ -1,14 +1,28 @@
 import axios from "axios";
 import { Platform } from "react-native";
 
-const emulatorIP = "10.0.2.2";
-const computerIP = "192.168.10.105";
+import { AppError } from "@/helpers/AppError";
+
+const isPhysicalDevice: boolean = true;
 
 const baseUrl = Platform.select({
   ios: "http://localhost:3001",
-  android: `http://${computerIP}:3001`,
+  android: isPhysicalDevice
+    ? "http://192.168.10.105:3001"
+    : "http://10.0.2.2:3001",
 });
 
 export const dtMoneyApi = axios.create({
   baseURL: baseUrl,
 });
+
+dtMoneyApi.interceptors.response.use(
+  (config) => config,
+  (error) => {
+    const message =
+      error.response?.data?.message ??
+      "Ocorreu um erro inesperado. Tente novamente.";
+
+    return Promise.reject(new AppError(message));
+  }
+);
